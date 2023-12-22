429394816c942e12a5a0b66cdf70c8818b360837
ankitd <dhimanankit262@gmail.com>
2023-12-22 13:43:52 +0530

    setup login and logout

--- dcrm/crm/views.py
+++ dcrm/crm/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    context = {
+        'title': 'Home'
+    }
+
+    if request.method == 'POST':
+        context['title'] = 'Log In'
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'You have been logged in. Redirecting to home...')
+            return redirect('home')
+        else:
+            messages.error(request,'There was an error logging in. Please try again...')
+            return redirect('home')
+    else:
+        return render(request,'crm/home.html',context)
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,'You have been logged out...')
+    return redirect('home')
